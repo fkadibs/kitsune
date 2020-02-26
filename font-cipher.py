efd1c1dd096ea35dedb07d6f223a11959f186982
disaster_byte <11808043+disasterbyte@users.noreply.github.com>
2020-02-26 12:25:39 -0600

    Update font-cipher.py

--- font-cipher.py
+++ font-cipher.py
@@ -8,36 +8,42 @@ import os
 if len(sys.argv) < 2:
     exit('Usage: font-cipher.py <filename> [text]')
 
+extension = sys.argv[1].split('.')[-1]
+
 # generate random substitutions
 normal = [l for l in ascii_letters]
 rand = [l for l in ascii_letters]
-shuffle(rand) # modify in place
-subs = { x: y for x, y in zip(normal, rand) }
+shuffle(rand)
+subs = { x: y for x, y in zip(normal, rand)}
 
-# convert source to xml
-print('[+] Loading file...')
-font = TTFont(sys.argv[1])
-font.saveXML('tmp.xml')
+if extension == 'ttf':
+    # convert source to xml
+    print('[+] Loading file...')
+    font = TTFont(sys.argv[1])
+    font.saveXML('output.ttx')
 
-# apply the substitutions
-print('[+] Applying substitutions...')
-font_tree = ET.parse('tmp.xml')
-font_root = font_tree.getroot()
-for letter in font_root.iter('map'):
-    if letter.attrib['name'] in subs.keys():
-        letter.set('name', subs[letter.attrib['name']])
-font_tree.write('tmp.xml')
+    # apply the substitutions
+    print('[+] Applying substitutions...')
+    font_tree = ET.parse('output.ttx')
+    font_root = font_tree.getroot()
+    for letter in font_root.iter('map'):
+        if letter.attrib['name'] in subs.keys():
+            letter.set('name', subs[letter.attrib['name']])
+    font_tree.write('output.ttx')
 
-# convert to ttf
-print('[+] Writing to disk...')
-font = TTFont()
-font.importXML('tmp.xml')
-font.save('output.ttf')
+    # convert to ttf
+    print('[+] Writing to disk...')
+    font = TTFont()
+    font.importXML('output.ttx')
+    font.save('output.ttf')
 
-# cleanup
-os.remove('tmp.xml')
+elif extension == 'ttx':
+    # load the xml
+    font_root = ET.parse(sys.argv[1]).getroot()
+    for letter in font_root.iter('map'):
+        if letter.attrib['name'] in normal:
+            # reverse map with hex key
+            subs[chr(int(''.join(letter.attrib['code'][-2::]), 16))] = letter.attrib['name']
 
 if len(sys.argv) > 2:
-    print('[+] Ciphertext:')
-    output_string = ''.join(subs[l] if l in subs.keys() else l for l in sys.argv[2])
-    print(output_string)
+    print('\n' + ''.join(subs[l] if l in subs.keys() else l for l in sys.argv[2]))
