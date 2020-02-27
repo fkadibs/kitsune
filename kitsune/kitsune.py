02494d64ab346492bf0e4761539193838cab8464
disaster_byte <11808043+disasterbyte@users.noreply.github.com>
2020-02-26 22:29:45 -0600

    Update kitsune.py

--- kitsune/kitsune.py
+++ kitsune/kitsune.py
@@ -1,14 +1,19 @@
 import xml.etree.ElementTree as ET	
-from fontTools.ttLib import TTFont	
+from fontTools.ttLib import TTFont, woff2
 from string import ascii_letters	
 from random import shuffle	
+import argparse
 import sys	
-import os	
+import os
 
-if len(sys.argv) < 2:	
-    exit('Usage: kitsune <filename> [text]')	
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', action='store', help='input filename')
+parser.add_argument('-c', action='store', metavar='TEXT', help='generate ciphertext')
+parser.add_argument('-o', action='store', metavar='FILE', default='output', help='output file name')
+parser.add_argument('-w', action='store_true', help='generate woff2 webfont')
+args = parser.parse_args()
 
-extension = sys.argv[1].split('.')[-1]	
+extension = args.filename.split('.')[-1]
 
 # generate random substitutions	
 normal = [l for l in ascii_letters]	
@@ -20,27 +25,30 @@ def main():
     if extension == 'ttf':	
         # convert source to xml	
         print('[+] Loading file...')	
-        font = TTFont(sys.argv[1])	
-        font.saveXML('output.ttx')	
+        font = TTFont(args.filename)	
+        font.saveXML(args.o + '.ttx')	
 
         # apply the substitutions	
         print('[+] Applying substitutions...')	
-        font_tree = ET.parse('output.ttx')	
+        font_tree = ET.parse(args.o + '.ttx')	
         font_root = font_tree.getroot()	
         for letter in font_root.iter('map'):	
             if letter.attrib['name'] in subs.keys():	
                 letter.set('name', subs[letter.attrib['name']])	
-        font_tree.write('output.ttx')	
+        font_tree.write(args.o + '.ttx')	
 
         # convert to ttf	
         print('[+] Writing to disk...')	
         font = TTFont()	
-        font.importXML('output.ttx')	
-        font.save('output.ttf')	
+        font.importXML(args.o + '.ttx')	
+        font.save(args.o + '.ttf')
+
+        if args.w:
+            woff2.compress(input_file=args.o +'.ttf', output_file=args.o + '.woff2')
 
     elif extension == 'ttx':	
         # load the xml	
-        font_root = ET.parse(sys.argv[1]).getroot()	
+        font_root = ET.parse(args.filename).getroot()	
         for letter in font_root.iter('map'):	
             if letter.attrib['name'] in normal:	
                 # reverse map keys by cmap hex code
@@ -48,8 +56,9 @@ def main():
                 ascii_key = chr(int(hex_code, 16))
                 subs[ascii_key] = letter.attrib['name']
 
-    if len(sys.argv) > 2:	
-        print('\n' + ''.join(subs[l] if l in subs.keys() else l for l in sys.argv[2]))
+    if args.c:	
+        print('\n' + ''.join(subs[l] if l in subs.keys() else l for l in args.c))
+
 
 if __name__ == '__main__':
     main()
