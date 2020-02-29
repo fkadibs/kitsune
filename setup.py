e7ce11e21a8c1aa9bd6e8e77336f863683bf769b
disaster_byte <11808043+disasterbyte@users.noreply.github.com>
2020-02-29 16:13:53 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name = 'kitsune',
-  version = '0.0.1',
+  version = '0.0.2',
   author = 'disaster_byte',
   install_requires = ['fonttools'],
   packages = ['kitsune'],
